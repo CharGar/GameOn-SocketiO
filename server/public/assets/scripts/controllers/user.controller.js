fb832edb5e6e065f80d3f921faafa7a786071c52
Charlie Garnaas <cgarnaas555@gmail.com>
2017-06-01 16:56:41 -0500

    added ng-click to markers

--- server/public/assets/scripts/controllers/user.controller.js
+++ server/public/assets/scripts/controllers/user.controller.js
@@ -1,10 +1,32 @@
 myApp.controller('UserController', ['$http', '$location', 'NgMap', function($http, $location, NgMap) {
   // This happens after view/controller loads -- not ideal but it works for now.
   var vm = this;
+    NgMap.getMap().then(function(map) {
+      console.log('map', map);
+      vm.map = map;
+    });
 
+    vm.clicked = function() {
+      alert('Clicked a link inside infoWindow');
+    };
+
+    vm.shops = [
+      {id:'foo', name: 'FOO SHOP', position:[41,-87]},
+      {id:'bar', name: 'BAR SHOP', position:[42,-86]}
+    ];
+    vm.shop = vm.shops[0];
+
+    vm.showDetail = function(e, shop) {
+      vm.shop = shop;
+      vm.map.showInfoWindow('foo-iw', shop.id);
+    };
+
+    vm.hideDetail = function() {
+      vm.map.hideInfoWindow('foo-iw');
+    };
   console.log('checking user');
     NgMap.getMap().then(function(map) {
-      
+
       vm.showCustomMarker = function(evt) {
         map.customMarkers.foo.setVisible(true);
         map.customMarkers.foo.setPosition(this.getPosition());
--- server/public/assets/scripts/newController.js
+++ server/public/assets/scripts/newController.js
@@ -1,13 +1,13 @@
-myApp.controller('MyCtrl', function(NgMap){
-       var vm = this;
-       console.log('in MyCtrl');
-    NgMap.getMap().then(function(map) {
-      vm.showCustomMarker= function(evt) {
-        map.customMarkers.foo.setVisible(true);
-        map.customMarkers.foo.setPosition(this.getPosition());
-      };
-      vm.closeCustomMarker= function(evt) {
-        this.style.display = 'none';
-         };
-       });
-     });
+// myApp.controller('MyCtrl', function(NgMap){
+//        var vm = this;
+//        console.log('in MyCtrl');
+//     NgMap.getMap().then(function(map) {
+//       vm.showCustomMarker= function(evt) {
+//         map.customMarkers.foo.setVisible(true);
+//         map.customMarkers.foo.setPosition(this.getPosition());
+//       };
+//       vm.closeCustomMarker= function(evt) {
+//         this.style.display = 'none';
+//          };
+//        });
+//      });
