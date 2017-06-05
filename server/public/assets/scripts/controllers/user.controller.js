d3c2063d62400f12f924bc38019282501d9d1db0
Charlie Garnaas <cgarnaas555@gmail.com>
2017-06-05 11:47:40 -0500

    added location imgs

--- server/public/assets/scripts/controllers/user.controller.js
+++ server/public/assets/scripts/controllers/user.controller.js
@@ -12,21 +12,33 @@ myApp.controller('UserController', ['$http', '$location', 'NgMap','$scope',
     vm.clicked = function() {
       alert("You're signed up to play, current roster: 1");
     };
-    
-    $scope.toggleLeft = buildToggler('left');
-    $scope.toggleRight = buildToggler('right');
 
-    function buildToggler(componentId) {
-      return function() {
-        $mdSidenav(componentId).toggle();
-      };
+    // vm.toggleLeft = buildToggler('left');
+    // vm.toggleRight = buildToggler('right');
+
+    vm.test = function(e, shop) {
+      // console.log('shop left', shopIndex);
+      vm.shop = shop;
+
+      $mdSidenav('left').toggle().then(function() {
+
+      });
+    }
+
+    vm.toggleLeft = function(componentId) {
+      // return function() {
+      console.log(componentId);
+        $mdSidenav(componentId).toggle().then(function() {
+          console.log('is open?', $mdSidenav(componentId).isOpen());
+        });
+      // };
     }
     vm.shops = [
-      {id:'grand', roster:' 0', Location: 'Grande Hotel Lifetime', nextGame: '11:30am', position:[44.975923, -93.268808]},
-      {id:'cross', roster:' 0', Location: 'Crosstown Lifetime', nextGame: '11:30am', position:[44.8897309, -93.4457767]},
-      {id:'louis', roster:' 0', Location: 'St Louis Park Lifetime', nextGame: '11:30am', position:[44.961798, -93.349895]},
-      {id:'ep', roster:' 0', Location: 'Eden Prairie Lifetime', nextGame: '11:30am', position:[44.85615401, -93.43660712]},
-      {id:'target', roster:' 0', Location: 'Target Center Lifetime', nextGame: '11:30am', position:[44.979527, -93.276157]}
+      {id:'grand', image:"url('/assets/styles/ghlt.jpg')", roster:' 0', Location: 'Grande Hotel Lifetime', nextGame: '11:30am', position:[44.975923, -93.268808]},
+      {id:'cross', image:"url('/assets/styles/ctlt.jpg')", roster:' 0', Location: 'Crosstown Lifetime', nextGame: '11:30am', position:[44.8897309, -93.4457767]},
+      {id:'louis', image:"url('/assets/styles/splt.jpg')", roster:' 0', Location: 'St Louis Park Lifetime', nextGame: '11:30am', position:[44.961798, -93.349895]},
+      {id:'ep', image:"url('/assets/styles/eplt.jpg')", roster:' 0', Location: 'Eden Prairie Lifetime', nextGame: '11:30am', position:[44.85615401, -93.43660712]},
+      {id:'target', image:"url('/assets/styles/tclt.jpg')", roster:' 0', Location: 'Target Center Lifetime', nextGame: '11:30am', position:[44.979527, -93.276157]}
     ];
     vm.shop = vm.shops[0];
 
@@ -50,25 +62,21 @@ myApp.controller('UserController', ['$http', '$location', 'NgMap','$scope',
         this.style.display = 'none';
       };
     });
-  }]);
+  $http.get('/user').then(function(response) {
+      if(response.data.username) {
+          // user has a curret session on the server
+          vm.userName = response.data.username;
+          console.log('User Data: ', vm.userName);
+      } else {
+          // user has no session, bounce them back to the login page
+          $location.path("/home");
+      }
+  });
 
-
-  // Upon load, check this user's session on the server
-//   $http.get('/user').then(function(response) {
-//       if(response.data.username) {
-//           // user has a curret session on the server
-//           vm.userName = response.data.username;
-//           console.log('User Data: ', vm.userName);
-//       } else {
-//           // user has no session, bounce them back to the login page
-//           $location.path("/home");
-//       }
-//   });
-//
-//   vm.logout = function() {
-//     $http.get('/user/logout').then(function(response) {
-//       console.log('logged out');
-//       $location.path("/home");
-//     });
-//   }
-// }]);
+  vm.logout = function() {
+    $http.get('/user/logout').then(function(response) {
+      console.log('logged out');
+      $location.path("/home");
+    });
+  }
+}]);
